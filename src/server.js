301d8891443eea8a00b8c331f0a93fa2437e9222
David Bong Xia Shing <davidbong@boostorder.com>
2024-10-31 17:07:56 +0800

    🧹: format code

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-const basicAuth = require('express-basic-auth');
+const basicAuth = require("express-basic-auth");
 require("dotenv").config();
 
 const app = express();
@@ -16,12 +16,12 @@ const apiSecret = process.env.API_SECRET;
 // Middleware
 app.use(cors());
 const auth = basicAuth({
-	authorizer: (key, secret) => {
-		return (key == apiKey) && secret == apiSecret;
-	},
-	unauthorizedResponse: () => {
-		return 'Unauthorized';
-	},
+  authorizer: (key, secret) => {
+    return key == apiKey && secret == apiSecret;
+  },
+  unauthorizedResponse: () => {
+    return "Unauthorized";
+  },
 });
 app.use(bodyParser.json());
 
@@ -36,13 +36,13 @@ app.use("/api/cart", auth, CartRoutes);
 app.use("/api/collection", auth, CollectionsRoutes);
 
 mongoose
-	.connect(uri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => console.log("MongoDB database Connected..."))
-	.catch((err) => console.log(err));
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB database Connected..."))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
